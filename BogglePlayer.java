3acfa386146b80a9bf88d6543db221173fb259d0
Tlex-Ahomas <alex.thomas@snet.net>
2023-11-27 11:55:25 -0500

    Basic Setup Created necessary methods with little to no bodies, brought in file and scanner imports, created header

--- BogglePlayer.java
+++ BogglePlayer.java
@@ -1,2 +1,22 @@
+/*
+* Author: C.A.M.E.R.O.N.
+* Course: CSE 2010
+* Section: 02
+* Description: given a dictionary and a Boggle board, finds the 20 highest scoring words
+ */
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class BogglePlayer {
+
+   public BogglePlayer (final String wordFile) throws FileNotFoundException {
+      final File words = new File(wordFile);
+      final Scanner scn = new Scanner(words, "US-ASCII");
+   }
+
+   public Word[] getWords(final char[][] board) {
+      return new Word[1];  // placeholder so class can compile
+   }
 }
